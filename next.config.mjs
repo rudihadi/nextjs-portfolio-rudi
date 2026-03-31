a38df11c26e32e47756d921929ed705a447344e9
Rudi Hadi <rudihadi5395@gmail.com>
2026-03-31 15:07:07 +0700

    rubah next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,32 @@
+// /** @type {import('next').NextConfig} */
+
+// const isProd = process.env.NODE_ENV === "production";
+
+// const nextConfig = {
+//   basePath: isProd ? "/rudi-hadi" : "",
+//   assetPrefix: isProd ? "/rudi-hadi" : "",
+//   images: {
+//     unoptimized: true,
+//   },
+//   trailingSlash: true,
+//   env: {
+//     NEXT_PUBLIC_BASE_PATH: isProd ? "/rudi-hadi" : "",
+//   },
+//   allowedDevOrigins: [
+//     "http://10.10.1.94:3000",
+//     "http://100.67.44.102:3000",
+//     "http://localhost:3000",
+//   ],
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  basePath: isProd ? "/rudi-hadi" : "",
-  assetPrefix: isProd ? "/rudi-hadi" : "",
   images: {
-    unoptimized: true,
+    unoptimized: true, // optional
   },
-  trailingSlash: true,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/rudi-hadi" : "",
-  },
-  allowedDevOrigins: [
-    "http://10.10.1.94:3000",
-    "http://100.67.44.102:3000",
-    "http://localhost:3000",
-  ],
 };
 
 export default nextConfig;
